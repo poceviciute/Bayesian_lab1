491cf0c406096d51cb169bdb2c0c205319f8d4fe
Fanny Karelius <fanny.karelius@telia.com>
2018-04-11 18:03:16 +0200

    lab1 r

--- lab1.R
+++ lab1.R
@@ -3,27 +3,29 @@
 #Question 1
 
 #a)
-y1<-rbeta(20, 2+14, 2+6)
-#true_mean <- 14/20
-#true_sd <- sqrt(true_mean*(1-true_mean))
 alpha <- 2+14
 beta <- 2+6
+y1<-rbeta(20, alpha, beta)
+
 t_mean <- alpha/(alpha+beta)
 t_sd <- sqrt(alpha*beta/((alpha+beta)^2*(alpha+beta+1)))
 samples = seq(20,10000,by=200)
 result = data.frame()
 for(i in 1:length(samples)){
-  temp = rbeta(samples[i], 2+14, 2+6)
- result[i,1] = mean(temp)
- result[i,2] <- sd(temp)
+  set.seed(12345)
+  temp <- rbeta(samples[i], alpha, beta)
+  result[i,1] <- mean(temp)
+  result[i,2] <- sd(temp)
 }
-result
-plot(result[,1], ylab="Mean", pch=19)
-lines(1:length(samples), rep(t_mean, 50))
-plot(result[,2], ylab="Standard deviation", pch=19)
-lines(1:length(samples), rep(t_sd, 50))
+colnames(result)<-c("Mean", "Sd")
+
+plot(samples, result$Mean, ylab="Mean", xlab="Sample size", main="Mean", pch=19)
+lines(samples, rep(t_mean, length(samples)))
+plot(samples, result$Sd, ylab="Standard deviation", xlab="Sample size", main="Standard deviation", pch=19)
+lines(samples, rep(t_sd, length(samples)))
 
 #b)
+set.seed(12345)
 y<-rbeta(10000, alpha, beta)
 prob <- length(y[y<0.4])/length(y)
 true_prob <- pbeta(0.4, alpha, beta)
@@ -32,10 +34,12 @@ true_prob <- pbeta(0.4, alpha, beta)
 logodds <- log(y/(1-y))
 hist(logodds, freq = FALSE)
 lines(density(logodds))
-hist(y)
-density(logodds)
-plot(density(logodds))
-hist(rnorm(10000, mean(logodds), sd(logodds)))
+
+# hist(y)
+# density(logodds)
+# plot(density(logodds))
+# set.seed(12345)
+# hist(rnorm(10000, mean(logodds), sd(logodds)))
 #looks normal
 
 #Question 2
@@ -45,7 +49,7 @@ x<-c(14, 25, 45, 25, 30, 33, 19, 50, 34, 67)
 mu <- 3.5
 n <- length(x)
 tau2 <- sum((log(x)-mu)^2)/n
-
+set.seed(12345)
 sim_sigma2 <- rchisq(10000,n)
 
 sigma2 <- n*tau2/sim_sigma2
@@ -59,41 +63,55 @@ inv_sd <- sqrt(2*inv_mean^2/(n-4))
 hist(sim_sigma2)
 
 #b)
-
+set.seed(12345)
 G <- 2*pnorm(sqrt(sigma2)/sqrt(2))-1
-
+dens_G<-density(G)
 lognorm = rlnorm(10000, mean(G), sd(G))
 hist(G, freq=FALSE)
-lines(density(G))
-dens_G<-density(G)
+lines(dens_G)
+
 
 #c)
 perc = 0.025*10000
-perc2 <- floor(0.025*length(dens_G$y))
 lowertail <- G[order(G, decreasing = FALSE)[perc+1]]
 uppertail <- G[order(G, decreasing = FALSE)[10000-perc-1]]
 
-dn <- cumsum(dens_G$y)/sum(dens_G$y)
-li <- which(dn>=0.05)[1]
-ui <- which(dn>=0.95)[1]
-dens_G$x[c(li,ui)]
+dataframe <- data.frame(y=dens_G$y,x=dens_G$x)
+dataframe <- dataframe[order(dataframe$y,decreasing = TRUE),]
+dataframe$dens <- cumsum(dataframe$y)/sum(dataframe$y)
+
+dfx<-dataframe$dens<0.95
+low<-which.min(dataframe$x[dfx])
+upp<-which.max(dataframe$x[dfx])
+x_low <- dataframe$x[low]
+x_upp <- dataframe$x[upp]
 
 hist(G, freq=FALSE)
-lines(density(G))
-lines(c(lowertail, uppertail), c(0,0), col="red", lwd=5)
-lines(dens_G$x[c(li,ui)], c(1,1), col="grey", lwd=5)
+lines(density(G), lwd=1)
+lines(c(lowertail, uppertail), c(0.3,0.3), col="grey48", lwd=5)
+lines(c(x_low, x_upp), c(1,1), col="grey20", lwd=5)
+legend(x = 0.6, y=5, c("HPD", "Equal tail"), col=c("grey20", "grey48"), lwd = 3)
 
-#3
+#Question 3
+
 #a)
 z <- c(-2.44, 2.14, 2.54, 1.83, 2.02, 2.33, -2.79, 2.23, 2.07, 2.02)
+len_z <- length(z)
 mu2 <- 2.39
 lambda <- 1
 
 posterior <- function(k){
-  result <- exp(k*sum(cos(z-mu2))-sum(z))/(2*pi*besselI(k,0))
+  result <- lambda*exp(k*sum(cos(z-mu2))-lambda*sum(z))/(2*pi*besselI(k,0))
   return(result)
 }
 
 ks <- seq(0,1,by=0.001)
 post <- posterior(ks)
 hist(post, freq = FALSE)
+lines(density(post))
+dens_post <- density(post)
+plot(dens_post)
+
+
+#b)
+post_mode <- max(dens_post$x)
